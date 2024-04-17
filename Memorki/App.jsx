37188e2d41748838b7b10d9e2fdedbe3734a873c
juliakow <u72_julkow_waw@technischools.com>
2024-04-17 10:07:41 +0200

    17.04.24

---
+++ Memorki/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return(
+        <>
+        <div className="App">
+            <h1>Harry Potter Memory</h1>
+            <button>New Game</button>
+        </div>
+        </>
+    )
+}
+
+export default App;
